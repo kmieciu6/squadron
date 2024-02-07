44f254ee1d902dc585908576debf59a6448ee4a2
JAKUB-PC\PC <jakub.kmiecik96@gmail.com>
2024-02-07 22:19:14 +0100

    contact form

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,16 @@
+import photo from "../assets/1.jpg";
 
 const About = () => {
 
     return (
         <div>
             <h1>About</h1>
+            <p>
+                Ea fugiat fugiat minim ipsum culpa excepteur laboris dolore elit ipsum ipsum fugiat. Et pariatur qui duis Lorem 
+                non non ipsum duis ex. Laboris ipsum aliqua fugiat quis tempor ullamco eiusmod commodo veniam ut excepteur nisi. 
+                Id elit tempor nostrud aute nulla eu sit et.
+            </p>
+            <img src={photo} alt="obraz"/>
         </div>
     )
 } 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,101 @@
+import React, { useState } from "react";
 
 const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        message: ''
+    });
+
+    const [errors, setError] = useState({});
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+        if (submitted) {
+            setError({
+                ...errors,
+                [name]: ''
+            });
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const errors = validateForm(formData);
+        if (Object.keys(errors).length === 0) {
+            console.log('Formularz przesłany: ', formData);
+            setError({});
+            setSubmitted(true);
+            setFormData({
+                name: '',
+                email: '',
+                phone: '',
+                message: ''
+            });
+        } else {
+            setError(errors);
+            setSubmitted(false);
+        }
+    };
+
+    const validateForm = (data) => {
+        const errors = {};
+        if (!data.name.trim()) {
+            errors.name = 'Pole jest wymagane';
+        } else if (data.name.trim().length < 2) {
+            errors.name = 'Imię musi zawierać co najmniej dwie litery'
+        }
+        if (!data.message.trim()) {
+            errors.message = 'Pole jest wymagane';
+        }
+        if (!data.email.trim()) {
+            errors.email = 'Email jest wymagany';
+        } else if (!isValidEmail(data.email)) {
+            errors.email = 'Nieprawidłowy adres email';
+        }
+        return errors;
+    };
+
+    const isValidEmail = (email) => {
+        // Prosta walidacja adresu email, można użyć bardziej zaawansowanych metod
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+      };
 
     return (
-        <div>
-            <h1>Contact</h1>
-        </div>
+        <section className="contact">
+            <h1>Kontakt</h1>
+            {submitted && Object.keys(errors).length === 0 && (
+                <p>Formularz został pomyślnie przesłany!</p>
+            )}
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">
+                    <p>Imię</p>
+                </label>
+                <input type="text" id="name" name="name" className="input" value={formData.name} onChange={handleChange}/>
+                {errors.name && <span className="error">{errors.name}</span>}
+                <label htmlFor="email">
+                    <p>Email</p>
+                </label>
+                <input type="text" id="email" name="email" className="input" value={formData.email} onChange={handleChange}/>
+                {errors.email && <span className="error">{errors.email}</span>}
+                <label htmlFor="phone">
+                    <p>Telefon</p>
+                </label>
+                <input type="text" id="phone" name="phone" className="input" value={formData.phone} onChange={handleChange}/>
+                <label htmlFor="message">
+                    <p>Wiadomość</p>    
+                </label>
+                <textarea id="message" name="message" className="input" value={formData.message} onChange={handleChange}/>
+                {errors.message && <span className="error">{errors.message}</span>}
+                <button type='submit'>Wyślij</button>
+            </form>
+        </section>
     )
 } 
 
