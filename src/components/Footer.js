d6d86078e3275b181033e0b7e7988f256960a1c1
JAKUB-PC\PC <jakub.kmiecik96@gmail.com>
2024-02-09 17:01:20 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,17 @@
+import facebook from "../assets/logo_facebook.png";
+import linkedIn from "../assets/logo_linkedIn.png";
 
 const Footer = () => {
     
     return (
-        <div>
+        <div className="footer">
             <h1>Logo</h1>
             <div>
                 <h4>Numer telefonu: +48 xxx xxx xxx</h4>
                 <h4>Mail: mail@mail.com</h4>
             </div>
+            <img src={facebook} alt="logo facebook" className="fb"/>
+            <img src={linkedIn} alt="logo linkedIn" className="in"/>
         </div>
     )
 };
