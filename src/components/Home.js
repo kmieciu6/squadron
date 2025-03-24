0b9ec511ab34a661f7820d24f38d1a11a78c2d7b
JAKUB-PC\PC <jakub.kmiecik96@gmail.com>
2025-03-24 18:26:09 +0100

    home change

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
 import { useState, useEffect } from 'react';
 import useIntersectionHide from './hooks/useIntersectionHide';
 import opening_photo from '../assets/dron2.jpg';
+import dron_photo from '../assets/dron.png';
 import turbine_photo from '../assets/turbiny.jpg';
+import soldier_photo from '../assets/soldier.png';
 import carousel1 from '../assets/dron.jpg';
 import carousel2 from '../assets/dron1.jpg';
 import carousel3 from '../assets/dron2.jpg';
-
+import { useLanguage } from './translations/LanguageContext';
+import { getTranslation } from "./translations/LanguageUtils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
     const [titleRef, isTitleHidden] = useIntersectionHide({ threshold: 0.8, rootMargin: "0px 0px -200px 0px" });
@@ -16,6 +22,7 @@ const Home = () => {
     const [sec5Ref, isSec5Hidden] = useIntersectionHide();
     const [currentIndex, setCurrentIndex] = useState(0);
     const [resetTimer, setResetTimer] = useState(false);
+    const { currentLanguage } = useLanguage();
 
     const slides = [
         {
@@ -69,35 +76,96 @@ const Home = () => {
 
         <div className="text text_container">
 
-            {/* <div className='text_background'> */}
+            <div className='text1_background'>
                 <div ref={sec1Ref} className={`text text1 text_width ${isSec1Hidden ? 'hidden' : ''}`}>
-                    <p>
-                        Do pariatur et elit cupidatat do ipsum mollit. Ullamco est do reprehenderit mollit. Eiusmod aliqua eiusmod nulla 
-                        aute id id irure incididunt dolore. Eiusmod anim duis labore elit nisi ex nulla. Quis officia ut et veniam proident 
-                        nulla reprehenderit magna pariatur culpa commodo. Reprehenderit velit consequat sint magna sunt dolor et ipsum 
-                        consequat Lorem nulla nostrud.
-                    </p>
+                    <div className='text1_container'>
+                        <h1>
+                            {getTranslation("unique_entity", currentLanguage)}
+                        </h1>
+                        <div>
+                            <p>
+                                {getTranslation("home_tex1.1", currentLanguage)}
+                            </p>
+                            <p>
+                                {getTranslation("home_tex1.2", currentLanguage)}
+                            </p>
+                            <NavLink to="/about" className="read_more_button">
+                                {getTranslation("read_more_about", currentLanguage) } 
+                                <FontAwesomeIcon icon={faArrowRight} className="icon" />
+                            </NavLink>
+                        </div>
+                    </div>
                 </div>
-            {/* </div> */}
+            </div>
 
-            <div ref={sec2Ref} className={`text text2 text_width ${isSec2Hidden ? 'hidden' : ''}`}>
-                <div className='image_container'>
-                    <img
-                        src={turbine_photo}
-                        alt="turbina"
-                    />
-                    <div className='text_overlay'>   
-                        <h4>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.    
-                        </h4>
-                    </div>                    
+            <div className='text2_background'>
+                <div ref={sec2Ref} className={`text text2 text_width ${isSec2Hidden ? 'hidden' : ''}`}>
+                    <h1>
+                        {getTranslation("areas", currentLanguage)}    
+                    </h1>
+                    <div className='images_container'>
+                        <div className='image'>
+                            <img
+                                src={dron_photo}
+                                alt="turbina"
+                            />
+                            <div className='text_overlay'>   
+                                <h3>
+                                    UAV
+                                </h3>
+                                <p>
+                                    Do pariatur et elit cupidatat do ipsum mollit. Ullamco est do reprehenderit mollit. Eiusmod aliqua eiusmod nulla 
+                                    aute id id irure incididunt dolore. Eiusmod anim duis labore elit nisi ex nulla. Quis officia ut et veniam proident 
+                                    nulla reprehenderit magna pariatur culpa commodo. Reprehenderit velit consequat sint magna sunt dolor et ipsum 
+                                    consequat Lorem nulla nostrud.
+                                </p>
+                                <button>
+                                    {getTranslation("more", currentLanguage)}
+                                </button>
+                            </div>                    
+                        </div>
+                        <div className='image'>
+                            <img
+                                src={turbine_photo}
+                                alt="turbina"
+                            />
+                            <div className='text_overlay'>   
+                                <h3>
+                                EKSPERTYZY OFFSHORE
+                                </h3>
+                                <p>
+                                    Do pariatur et elit cupidatat do ipsum mollit. Ullamco est do reprehenderit mollit. Eiusmod aliqua eiusmod nulla 
+                                    aute id id irure incididunt dolore. Eiusmod anim duis labore elit nisi ex nulla. Quis officia ut et veniam proident 
+                                    nulla reprehenderit magna pariatur culpa commodo. Reprehenderit velit consequat sint magna sunt dolor et ipsum 
+                                    consequat Lorem nulla nostrud.
+                                </p>
+                                <button>
+                                    {getTranslation("more", currentLanguage)}
+                                </button>
+                            </div>         
+                        </div>
+                        <div className='image'>
+                            <img
+                                src={soldier_photo}
+                                alt="turbina"
+                            />
+                            <div className='text_overlay'> 
+                                <h3>
+                                    SOFT
+                                </h3>  
+                                <p>
+                                    Do pariatur et elit cupidatat do ipsum mollit. Ullamco est do reprehenderit mollit. Eiusmod aliqua eiusmod nulla 
+                                    aute id id irure incididunt dolore. Eiusmod anim duis labore elit nisi ex nulla. Quis officia ut et veniam proident 
+                                    nulla reprehenderit magna pariatur culpa commodo. Reprehenderit velit consequat sint magna sunt dolor et ipsum 
+                                    consequat Lorem nulla nostrud.
+                                </p>
+                                <button>
+                                    {getTranslation("more", currentLanguage)}
+                                </button>
+                            </div>         
+                        </div>
+                    </div>
                 </div>
-                <p>
-                    Do pariatur et elit cupidatat do ipsum mollit. Ullamco est do reprehenderit mollit. Eiusmod aliqua eiusmod nulla 
-                    aute id id irure incididunt dolore. Eiusmod anim duis labore elit nisi ex nulla. Quis officia ut et veniam proident 
-                    nulla reprehenderit magna pariatur culpa commodo. Reprehenderit velit consequat sint magna sunt dolor et ipsum 
-                    consequat Lorem nulla nostrud.
-                </p>
             </div>
 
             <div ref={sec3Ref} className={`text text3 text_width ${isSec3Hidden ? 'hidden' : ''}`}>
