f5bca92a7d8ba6c88d8d77697bc73e5561783ba0
jakub <jakub.kmiecik96@gmail.com>
2025-05-02 00:46:37 +0200

    spinner change

--- src/components/PageLoader.js
+++ src/components/PageLoader.js
@@ -3,52 +3,21 @@ import React, { useState, useEffect } from 'react';
 const LoadingSpinner = () => <div className='loading-spinner'></div>;
 
 const PageLoader = ({ children }) => {
-    const [isPageLoading, setIsPageLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const handleLoad = () => {
-            setIsPageLoading(false);
-        };
-
-        if (document.readyState === 'complete') {
-            handleLoad();
-        } else {
-            window.addEventListener('load', handleLoad);
+    useEffect (() => {
+        const onLoad = () => setLoading(false)
+        if (document.readyState  === 'complete') {
+            onLoad();
+        } else { 
+            window.addEventListener('load', onLoad);
+            return () => window.removeEventListener('load', onLoad);
         }
-
-        return () => window.removeEventListener('load', handleLoad);
-    }, []);
-
-    useEffect(() => {
-        const resources = Array.from(document.querySelectorAll('img, script, link[rel="stylesheet"]'));
-        const totalResources = resources.length;
-        let loadedResources = 0;
-
-        const handleResourceLoad = () => {
-            loadedResources++;
-            if (loadedResources === totalResources) {
-                setIsPageLoading(false);
-            }
-        };
-
-        resources.forEach(resource => {
-            if ((resource.tagName === 'IMG' || resource.tagName === 'LINK' || resource.tagName === 'SCRIPT') && resource.readyState === 'complete') {
-                handleResourceLoad();
-            } else {
-                resource.addEventListener('load', handleResourceLoad);
-            }
-        });
-
-        return () => {
-            resources.forEach(resource => {
-                resource.removeEventListener('load', handleResourceLoad);
-            });
-        };
     }, []);
 
     return (
         <div>
-            {isPageLoading && (
+            {loading && (
                 <div className='overlay'>
                     <LoadingSpinner />
                 </div>
