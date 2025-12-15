6b5c4ba5cabe57267b1170b24f0c255d054fb315
Jakub <jakub.kmiecik96@gmail.com>
2025-12-15 13:32:24 +0100

    changed styles

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,7 +1,6 @@
 'use client';
 import useTranslation from './hooks/useTranslation';
 import Link from "next/link";
-// import img_page_not_found from '../../public/images/page_not_found.jpg'
 import Image from 'next/image';
 
 const NotFoundPage = () => {
@@ -14,7 +13,6 @@ const NotFoundPage = () => {
                 {t('not_found_page')}
             </p>
             <Link href='/'><button>{t('main_page')}</button></Link>
-            {/*<img src={img_page_not_found.src} alt='not found'/>*/}
             <Image
                 src="/images/page_not_found.jpg"
                 alt="not found"
